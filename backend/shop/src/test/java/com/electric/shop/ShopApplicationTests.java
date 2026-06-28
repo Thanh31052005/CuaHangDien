9254f5524a0f19a60be6551b23b0ade67cdb070f
Pham Duy Thanh <phamduythanh31052005@gmail.com>
2026-06-28 22:29:45 +0700

    first commit

---
+++ backend/shop/src/test/java/com/electric/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electric.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
